a7c0f8c88a359118cbc3f2f46373d638262d5b17
Impanda9 <137157462+Impanda9@users.noreply.github.com>
2024-01-23 08:47:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const answerButtons = document.querySelectorAll('button[name="answer"]');
+const submitButton = document.querySelector('.submit-button');
+
+answerButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    answerButtons.forEach(btn => btn.classList.remove('selected'));
+    button.classList.add('selected');
+  });
+});
+
+document.getElementById("quiz-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const selectedButton = document.querySelector('button[name="answer"].selected');
+
+  if (selectedButton) {
+    const answerValue = selectedButton.value;
+
+    localStorage.setItem("Oldest employe", answerValue);
+    console.log("Selected answer registered in local storage:", answerValue);
+
+    window.location.href = "7.MissNR.html";
+  } else {
+    console.log("No answer selected");
+  }
+});
